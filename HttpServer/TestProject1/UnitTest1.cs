eee11515dc9a8b42a66c3c1eb651e63fac420bd0
Dannissimo20 <Denicha_d_89@mail.ru>
2023-10-12 21:18:28 +0300

    update: configuration of environment

---
+++ HttpServer/TestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Flurl;
+using Flurl.Http;
+namespace TestProject1;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup() {}
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
