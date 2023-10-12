7bc0b13c389c5f03f9cfc74e659e0e89e9aada92
Dannissimo20 <Denicha_d_89@mail.ru>
2023-10-12 22:10:24 +0300

    update: TimeTableService realization

--- OstApp/OstLib/Services/TimeTableService.cs
+++ OstApp/OstLib/Services/TimeTableService.cs
@@ -1,7 +1,27 @@
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using OstLib.Repository;
+
 namespace OstLib.Services
 {
-    public class TimeTableService
+    public class TimeTableService : ITimeTable
     {
-        
+        private readonly ApplicationContext _context;
+        public TimeTableService(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<TimeTableEntry> FindAll()
+        {
+            /*
+             * AsNoTracking() - позволяет не отслеживать изменения в бд
+             * Ускоряет работу EntityFramework
+             *
+             * Include() - подгружает вложенный объект указанный в лямюде
+             * Делает join при обращении к бд
+             */
+            return _context.TimeTableEntry.AsNoTracking().Include(t => t.Client);
+        }
     }
 }
