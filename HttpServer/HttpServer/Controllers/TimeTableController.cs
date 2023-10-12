fc098344d693aa064515e07cf3cdda36d7ae2722
Dannissimo20 <Denicha_d_89@mail.ru>
2023-10-12 22:13:07 +0300

    fix: TimeTableController loads Client

--- HttpServer/HttpServer/Controllers/TimeTableController.cs
+++ HttpServer/HttpServer/Controllers/TimeTableController.cs
@@ -1,6 +1,7 @@
 using HttpServer.Models;
 using OstLib;
 using Microsoft.AspNetCore.Mvc;
+using OstLib.Repository;
 
 namespace HttpServer.Controllers
 {
@@ -8,10 +9,16 @@ namespace HttpServer.Controllers
     [ApiController]
     public class TimeTableController : ControllerBase
     {
+        private readonly ITimeTable _timeTable;
+        public TimeTableController(ITimeTable timeTable)
+        {
+            _timeTable = timeTable;
+        }
+        
         [HttpGet]
         public IEnumerable<TimeTableEntryModel> GetTimeTable()
         {
-            var listOfItems = TimeTableEntry
+            var listOfItems = _timeTable
             .FindAll()
             .Select(s => new TimeTableEntryModel(
                     s.DateTime, 
--- HttpServer/HttpServer/Program.cs
+++ HttpServer/HttpServer/Program.cs
@@ -1,9 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using OstLib;
+using OstLib.Repository;
+using OstLib.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
 var connection = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationContext>(ac => ac.UseNpgsql(connection));
+
+builder.Services.AddTransient<ITimeTable, TimeTableService>();
 
 builder.Services.AddControllers();
 
