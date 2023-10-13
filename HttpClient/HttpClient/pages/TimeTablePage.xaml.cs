1618a2a49bac89a9883c6cd387caf233cfc9f153
Dannissimo20 <Denicha_d_89@mail.ru>
2023-10-13 17:47:50 +0300

    add: configuration project, installing NuGet packages

---
+++ HttpClient/HttpClient/pages/TimeTablePage.xaml.cs
@@ -0,0 +1,13 @@
+using System.Windows.Controls;
+
+namespace HttpClient.pages
+{
+    public partial class TimeTablePage : Page
+    {
+        public TimeTablePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
---
+++ HttpClient/HttpClient/windows/AddTimeTableLineWindow.xaml.cs
@@ -0,0 +1,13 @@
+using System.Windows;
+
+namespace HttpClient.windows
+{
+    public partial class AddTimeTableLineWindow : Window
+    {
+        public AddTimeTableLineWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
