a5d0251b3d38dc999122cca42ea6db5933d2817d
Dannissimo20 <Denicha_d_89@mail.ru>
2023-10-15 00:58:08 +0300

    update: POST method AddTimeTableLine

--- HttpServer/HttpServer/Controllers/TimeTableController.cs
+++ HttpServer/HttpServer/Controllers/TimeTableController.cs
@@ -11,21 +11,23 @@ namespace HttpServer.Controllers
     public class TimeTableController : ControllerBase
     {
         private readonly ITimeTable _timeTable;
-        public TimeTableController(ITimeTable timeTable)
+        private readonly IClient _client;
+        public TimeTableController(ITimeTable timeTable, IClient client)
         {
             _timeTable = timeTable;
+            _client = client;
         }
-        
+
         [HttpGet]
         public IEnumerable<TimeTableEntryModel> GetTimeTable()
         {
             var listOfItems = _timeTable
             .FindAll()
             .Select(s => new TimeTableEntryModel(
-                    s.DateTime, 
-                    s.Client.Surname, 
-                    s.Client.Name, 
-                    s.Client.PhoneNumber));
+                s.DateTime,
+                s.Client.Surname,
+                s.Client.Name,
+                s.Client.PhoneNumber));
             return listOfItems;
         }
 
@@ -35,9 +37,9 @@ namespace HttpServer.Controllers
             var listOfItems = _timeTable
             .FindAllForThisWeek(timeTableWeekModel.I)
             .Select(s => new TimeTableEntryModel(
-                s.DateTime, 
-                s.Client.Surname, 
-                s.Client.Name, 
+                s.DateTime,
+                s.Client.Surname,
+                s.Client.Name,
                 s.Client.PhoneNumber));
             return listOfItems;
         }
@@ -45,7 +47,38 @@ namespace HttpServer.Controllers
         [HttpPost]
         public int AddTimeTableLine(TimeTableLineModel tableLineModel)
         {
-            
+            try
+            {
+                Client client;
+                TimeTableEntry timeTableLine;
+                if (tableLineModel.ID == 0)
+                {
+                    client = new Client(
+                        tableLineModel.Surname,
+                        tableLineModel.Name,
+                        "",
+                        tableLineModel.Phone,
+                        "0",
+                        "-",
+                        "",
+                        "",
+                        "",
+                        "");
+                    _client.Add(client);
+                }
+                else
+                    client = _client.GetById(tableLineModel.ID);
+                timeTableLine = new TimeTableEntry(
+                    DateTime.Parse(tableLineModel.Date),
+                    client);
+                _timeTable.Add(timeTableLine);
+                return 200;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("ОБОШИБКА " + e.Message);
+                return 400;
+            }
         }
     }
 }
--- OstApp/OstLib/Models/TimeTableLineModel.cs
+++ OstApp/OstLib/Models/TimeTableLineModel.cs
@@ -3,6 +3,7 @@ namespace OstLib
     public class TimeTableLineModel
     {
         public string Date { get; set; }
+        public int ID { get; set; }
         public string Surname { get; set; }
         public string Name { get; set; }
         public string Phone { get; set; }
