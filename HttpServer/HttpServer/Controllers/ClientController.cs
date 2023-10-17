9da4f3ec3206f1feadc62d7efc76fa67008d3948
Dannissimo20 <Denicha_d_89@mail.ru>
2023-10-17 18:44:36 +0300

    fix: ApiController istead Controller

--- HttpServer/HttpServer/Controllers/ClientController.cs
+++ HttpServer/HttpServer/Controllers/ClientController.cs
@@ -5,7 +5,7 @@ using OstLib.Repository;
 namespace HttpServer.Controllers
 {
     [Route("Client")]
-    [Controller]
+    [ApiController]
     public class ClientController : ControllerBase
     {
         private readonly IClient _client;
@@ -17,9 +17,6 @@ namespace HttpServer.Controllers
         [HttpPost("getBySurname")]
         public IEnumerable<Client> GetClientsBySurname(SurnameModel surnameModel)
         {
-            /*
-             * TODO: surnameModel возвращает null при не null-овом значении. Нужно исправить
-             */
             if (surnameModel.Surname == null || surnameModel.Surname == "")
                 return _client.FindAll();
             var list = _client.FindClientsBySurname(surnameModel.Surname);
