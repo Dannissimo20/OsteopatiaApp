880b965b05e90aa07333ca9566e6d7f22aaa8eec
Dannissimo20 <Denicha_d_89@mail.ru>
2023-10-18 15:01:52 +0300

    add: more model replaced from OstLib

---
+++ HttpClient/HttpClient/models/TimeTableEntry.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Castle.Core.Internal;
+
+namespace HttpClient
+{
+    public class TimeTableEntry
+    {
+        public int ID { get; set; }
+        public DateTime DateTime { get; set; }
+        public virtual Client Client { get; set; }
+
+        public TimeTableEntry(){}
+
+        public TimeTableEntry(DateTime DateTime, Client Client)
+        {
+            this.DateTime = DateTime;
+            this.Client = Client;
+        }
+
+        public string GetDate
+        {
+            get
+            {
+                return DateTime.ToString("f");
+            }
+        }
+        
+        public string GetTime
+        {
+            get
+            {
+                return DateTime.ToString("t");
+            }
+        }
+    }
+}
